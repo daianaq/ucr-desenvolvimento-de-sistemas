c4256678a3785d4fe33a58505ac3bf210a4b46ee
daianaq <daianafreitas237@gmail.com>
2025-10-21 22:50:19 -0300

    Envio projeto um

---
+++ projetoUm/projetoUm/src/main/java/com/example/projetoUm/ProjetoUmApplication.java
@@ -0,0 +1,19 @@
+package com.example.projetoUm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoUmApplication.class, args);
+	}
+
+
+
+
+
+
+
+}
---
+++ projetoUm/projetoUm/src/main/java/com/example/projetoUm/model/ClienteModel.java
@@ -0,0 +1,48 @@
+package com.example.projetoUm.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ClienteModel {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    public ClienteModel(){
+    }
+
+    public ClienteModel(Long id, String nome, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
